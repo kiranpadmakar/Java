8d56ef90e979a0b21974bb7cc296c5c82a85a512
Kiran Padmakar <Kiran_Kadiyala@ad.infosys.com>
2018-08-30 10:09:56 +1000

    Pipelinefile

---
+++ MyJavaPipe.groovy
@@ -0,0 +1,35 @@
+pipeline {
+	agent any
+	tools {
+		ant 'Ant Installation'
+		jdk 'Java'
+	}
+	environment {
+		GH_ACCESS_KEY = credentials('GitHub_KP_Cred')
+	}
+	stages {
+		stage('Welcome'){
+			steps {
+				echo "Hello World!!! Welcome to my Java Build"
+			}
+		}
+		stage('build'){
+			steps {
+				git {
+					branch "myJavaLocal"
+					credentialsId "GitHub_KP_Cred"
+					url "https://github.com/kiranpadmakar/Java.git"
+				}
+			}
+			steps {
+				withAnt {
+					installation "Ant Installation"
+					jdk "Java"
+					target "main"
+				}
+			}
+		}
+	
+	}
+
+}
